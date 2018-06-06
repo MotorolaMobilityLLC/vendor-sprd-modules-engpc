f42ec096b2c77ac9376e5d04fa9048b7748f46cc
qingjun.yu <qingjun.yu@spreadtrum.com>
2018-06-06 16:55:40 +0800

    Bug #873681 engpc bring up
    [root cause  ] engpc bring up
    [changes     ] engpc bring up
    [side effects] no
    [self test   ]:
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sp9820e_1h10
    [test case]:poweron, enter cali mode
    [reviewers   ] ningbiao.huang,huoming.zhang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ia421715cd0fe3117772284fc96325f9775b9331d

--- eng_list.h
+++ eng_list.h
@@ -2,11 +2,11 @@
 #define _ENG_LIST_H
 
 
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define _offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 
 #define container_of(ptr, type, member) ( { \
 const typeof( ((type *)0)->member ) *__mptr = (ptr); \
-(type *)( (char *)__mptr - offsetof(type,member) ); } )
+(type *)( (char *)__mptr - _offsetof(type,member) ); } )
 
 static inline void prefetch(const void *x) {;}
 static inline void prefetchw(const void *x) {;}

2d4a1066ca34df41cef0b805dfe9a8cc1fd3d335
qingjun.yu <qingjun.yu@spreadtrum.com>
2018-09-18 15:06:51 +0800

    Bug #873681 use define tee
    [root cause  ] use define tee
    [changes     ] use define tee
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:yes
    [confirm dependent commit]:yes
    [board]:sp9820e_1h10, sp7731e_1h10
    [test case]:poweron, enter cali
    [reviewers   ] huoming.zhang,ningbiao.huang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ib4f58016079d64f78ca316d6ecbb0c2ceab84d7b

--- Android.mk
+++ Android.mk
@@ -20,18 +20,18 @@ else
 
 LOCAL_SHARED_LIBRARIES  := libcutils libsqlite libhardware libhardware_legacy libatci libefuse libbm 
 
-#ifeq ($(strip $(BOARD_TEE_CONFIG)), watchdata)
+ifeq ($(strip $(BOARD_TEE_CONFIG)), watchdata)
 LOCAL_CFLAGS += -DTEE_PRODUCTION_CONFIG
 LOCAL_SHARED_LIBRARIES  += libteeproduction
-#else ifeq ($(strip $(BOARD_TEE_CONFIG)), beanpod)
+else ifeq ($(strip $(BOARD_TEE_CONFIG)), beanpod)
 LOCAL_CFLAGS += -DTEE_PRODUCTION_CONFIG
 LOCAL_SHARED_LIBRARIES  += libteeproduction
-#else ifeq ($(strip $(BOARD_TEE_CONFIG)), trusty)
-#ifeq ($(strip $(TRUSTY_PRODUCTION)),true)
+else ifeq ($(strip $(BOARD_TEE_CONFIG)), trusty)
+ifeq ($(strip $(TRUSTY_PRODUCTION)),true)
 LOCAL_CFLAGS += -DTEE_PRODUCTION_CONFIG
 LOCAL_SHARED_LIBRARIES  += libteeproduction
-#endif
-#endif
+endif
+endif
 endif
 
 LOCAL_SHARED_LIBRARIES += libtracedump

6af7eff003cb745788ac97dbe54e576ceb9e849d
freeman.liu <Freeman.Liu@spreadtrum.com>
2019-08-12 15:22:22 +0800

    Bug #1141146 add tsensor module
    [root cause  ] project need
    [changes     ] add tsensor module
    [side effects] no
    [self test   ]: build ok
    [download normally]: ok
    [power on/off normally]: ok
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: sharkl5pro
    [test case]: build ok
    [reviewers   ] panvs.zhong
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Change-Id: Ia29f1fc47f57316cdd613101772516ea30a0bbd0

---
+++ modules/tsensor/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_32_BIT_ONLY := true
+
+LOCAL_SRC_FILES := tsensor.c
+
+LOCAL_MODULE := libtsensor
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := npidevice
+
+LOCAL_C_INCLUDES:= \
+	$(TOP)/vendor/sprd/proprietories-source/engmode \
+	$(TOP)/vendor/sprd/proprietories-source/engmode/tsensor
+
+LOCAL_SHARED_LIBRARIES:= liblog libc libcutils
+
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)

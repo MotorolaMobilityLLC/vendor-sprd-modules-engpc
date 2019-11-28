c5de526316b9bddda70df941b7cb50e3a6858eaf
bao.yue <bao.yue@unisoc.com>
2019-11-28 11:29:53 +0800

    Bug #1087018 Support AT command "AT+CCLK?"
    [root cause  ] the time of modem isn't synchronous with AP
    [changes     ] engpc deal with the command
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums312_1h10_ctcc_nosec-userdebug-native_SHARKL5_nosec_Uncache
    [test case]:est the command via ARMLOG
    [reviewers   ] alvin.zhou ziyi.zhang
    [change_type ] feature_bugfix
    [tag_product ] specific
    
    Change-Id: I2f880e82a45652d3fe9b07693229e9da164ed8f6

--- modules/libcomcontrol/com_control.h
+++ modules/libcomcontrol/com_control.h
@@ -3,4 +3,8 @@
 
 int open_diag_port (char *req, char *rsp);
 int close_diag_port (char *req, char *rsp);
+int cclk_hander(char* req,char* rsp);
+int parse_number(const uint8_t* data,size_t len ,unsigned& num);
+int parse_plus_cclk(char* str, int num);
+int get_calend_time(time_t tnow, struct tm* lt, int* tz);
 #endif

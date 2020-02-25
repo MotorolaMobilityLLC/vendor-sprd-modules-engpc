153bb24dfa5af36f0932495a373789a9fa9df327
xuanchen.jiang <xuanchen.jiang@spreadtrum.com>
2020-02-25 14:18:31 +0800

    Bug #1252584 engpc bringup, cali mode not working
    [root cause  ] cali mode not working
    [changes     ] modify mk file
    [side effects] none
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:9863a
    [test case]:engpc enter cali mode
    [reviewers   ]
    [change_type ] bringup --> build_config
    [tag_product ] common
    
    Change-Id: I02cd1c1b78a1096915044b6b4f9304df5c5841b9

--- server/adapt/android/include.mk
+++ server/adapt/android/include.mk
@@ -2,6 +2,7 @@
 file_list += $(wildcard $(LOCAL_PATH)/android/*.cpp)
 LOCAL_C_INCLUDES    += $(LOCAL_PATH)/android/
 
-ifeq ($(PLATFORM_VERSION),10)
+ifneq ($(filter $(strip $(PLATFORM_VERSION)),Q 10 R 11),)
     LOCAL_CFLAGS += -DENGPC_VSER_INIT
 endif
+

b0dffc7e16a61ccf6b2578352793378e6b53263a
ontimoss <os_github@163.com>
2023-08-10 10:49:58 +0800

    delete some files

--- server/adapt/android/include.mk
+++
@@ -1,8 +0,0 @@
-
-file_list += $(wildcard $(LOCAL_PATH)/android/*.cpp)
-LOCAL_C_INCLUDES    += $(LOCAL_PATH)/android/
-
-ifneq ($(filter $(strip $(PLATFORM_VERSION)),Q 10 R 11),)
-    LOCAL_CFLAGS += -DENGPC_VSER_INIT
-endif
-
